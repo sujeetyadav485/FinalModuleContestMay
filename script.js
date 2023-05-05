7aebf0efad7af9846a36188ae3f8d70d3ffb74a1
Sujeet Yadav <99132296+sujeetyadav485@users.noreply.github.com>
2023-05-05 21:44:28 +0530

    SubmitContest

---
+++ script.js
@@ -0,0 +1,121 @@
+var ipAddr = "";
+var ipTag = document.querySelector("h3");
+var token="c05a34b7ad1ede";
+var ipData="";
+var map=document.getElementById("map");
+var lat="";
+var lon="";
+var postalData=""
+
+var postalElement=document.querySelector(".postal-data");
+
+
+// Get user's IP address
+
+async function getIp() {
+  ipAddr = await fetch("https://ipinfo.io/47.15.38.155/json?token=c05a34b7ad1ede")
+    .then(response => response.json())
+    .then(data =>data.ip);
+}
+
+
+
+getIp()
+  .then(() => {
+    ipTag.innerText += ipAddr;
+})
+
+
+// Get user's location  
+
+
+async function getData(){
+    ipData=await fetch(`https://ipinfo.io/${ipAddr}?token=${token}`).then((response)=>response.json()).then((data)=>ipData=data);
+    lat=ipData.loc.split(",")[0];
+    lon=ipData.loc.split(",")[1];
+    console.log(lat,lon);
+}
+
+// Display user's local time
+
+function getCurrentTime(timeZone){
+    var now = new Date();
+
+  
+  var options = {
+    timeZone: timeZone,
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric'
+  };
+
+  // Format the date and time according to the options
+  const formatter = new Intl.DateTimeFormat([], options);
+  var dateAndTime = formatter.format(now);
+
+  // Return the formatted date and time
+  return dateAndTime;
+}
+
+
+// Search for post offices
+
+
+async function getPostalData(pin){
+ return  await fetch(`https://ipinfo.io/47.15.38.155/json?token=c05a34b7ad1ede${pin}`).then((response)=>response.json());
+}
+
+document.getElementById("getData").addEventListener("click", (e) => {
+  var button = e.target;
+  button.style.display = "none";
+  document.querySelector(".second").style.display="block"
+  getData().then(()=>{console.log(ipData);
+    document.getElementById("lat").innerHTML+=lat;
+    document.getElementById("long").innerHTML+=lon;
+    document.getElementById("city").innerHTML+=ipData.city;
+    document.getElementById("org").innerHTML+=ipData.org;
+    document.getElementById("region").innerHTML+=ipData.region;
+    document.getElementById("host").innerHTML+=ipData.hostname;
+    document.getElementById("time").innerHTML+=ipData.timezone;
+    document.getElementById("pin").innerHTML+=ipData.postal;
+    document.getElementById("date").innerHTML+=getCurrentTime(ipData.timezone);
+
+    getPostalData(ipData.postal).then((data)=>{
+        postalData=data[0];
+        document.getElementById("message").innerHTML+=postalData.Message;
+        console.log(postalData);
+        renderItems(postalData.PostOffice)
+    })
+
+    map.src=`https://maps.google.com/maps?q=${lat},${lon}&z=15&output=embed`;
+
+
+});
+  
+});
+
+function renderItems(data){
+    var innerHtml="";
+    postalElement.innerHTML=""
+    data.forEach((i)=>{
+       innerHtml+=`<div class="item">
+        <p>Name : ${i.Name}</p>
+        <p>Branch Type : ${i.BranchType}</p>
+        <p>Delivery Status : ${i.DeliveryStatus}</p>
+        <p>District : ${i.District}</p>
+        <p>Division : ${i.Division}</p>
+    </div>`
+    })
+
+    postalElement.innerHTML+=innerHtml
+}
+
+document.getElementById("search").addEventListener("input",(e)=>{
+    var input=e.target.value.trim();
+    renderItems(postalData.PostOffice.filter((i)=>i.Name.toLowerCase().includes(input) || i.BranchType.toLowerCase().includes(input)))
+
+})
